f7db3dcf663b1b2928d27a0bee70c5085c1aa0c6
Steven Schwab <schwabs@umich.edu>
2022-12-15 18:23:15 -0600

    Initial Commit

---
+++ Apple-Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple-Frameworks
+//
+//  Created by Steven Schwab on 12/15/22.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
